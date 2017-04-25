c18a777b39f13f1f19e97ae87ac90957f36dee53
George Preudhomme <Achilles@rescomp-16-289183.stanford.edu>
2017-04-25 01:24:27 -0700

    Fixed focus heuristic, added statesReachable heuristic

--- ggp-base-master/src/main/java/org/ggp/base/player/gamer/statemachine/GoatsIterativeDeep.java
+++ ggp-base-master/src/main/java/org/ggp/base/player/gamer/statemachine/GoatsIterativeDeep.java
@@ -24,7 +24,8 @@ public class GoatsIterativeDeep extends the_men_who_stare_at_goats {
 		roles = machine.getRoles();
 		self_index = roles.indexOf(role);
 		mob_n = 0;
-		weights = new double[]{0.75, 0.125, 0.125};
+		self_focus = false; //if set to true, heuristic will try to limit player's mobility
+		weights = new double[]{0.75, 0.0125, 0.0125};
 
 	}
 
@@ -48,7 +49,7 @@ public class GoatsIterativeDeep extends the_men_who_stare_at_goats {
 			for(List<Move> jointMove : jointMoves) {
 				int nextPlayer = self_index + 1;
 				MachineState nextState = machine.getNextState(state, jointMove);
-				int result = alphabeta(nextPlayer % roles.size(), nextState, 0, 100, depth, WEIGHTED_FN);
+				int result = alphabeta(nextPlayer % roles.size(), nextState, 0, 100, depth);
 				System.out.println(jointMove.get(self_index) + " " + result);
 				if(result == 100) {
 					return jointMove.get(self_index);
@@ -67,42 +68,125 @@ public class GoatsIterativeDeep extends the_men_who_stare_at_goats {
 				bestActions = actions;
 			}
 			System.out.println(num_actions + " Actions Deliberated of " + jointMoves.size() + " actions");
-			if(System.currentTimeMillis() > finishBy) break;
+			if(System.currentTimeMillis() >= finishBy) break;
 			depth += 1;
 		}
 		Random rn = new Random();
 		return bestActions.get(rn.nextInt(bestActions.size()));
 	}
 
-	protected int nStepMobilityFn(int player, MachineState state, int n) throws MoveDefinitionException, TransitionDefinitionException {
-		origin_player = player;
-		int mobility = (int) (100 * (nStepActions(player, state, n) /
-				machine.findActions(roles.get(player)).size()));
-		//System.out.println("Mobility: " + mobility);
-		return mobility;
 
-		//HashSet<MachineState> uniqueStates = new HashSet<MachineState>();
-		//statesReachable(player, state, n, uniqueStates);
-		//How to map to {0, ..., 100}?
-		//return uniqueStates.size()
+	protected int alphabeta(int player, MachineState state, int alpha, int beta, int d) throws MoveDefinitionException, GoalDefinitionException, TransitionDefinitionException {
+		if (machine.isTerminal(state))
+			return machine.getGoal(state, roles.get(self_index));
+		if (d == 0)
+			return evalFn(player, state, FOCUS_FN);
+
+		List<List<Move>> jointMoves = machine.getLegalJointMoves(state);
+		int score;
+		if (player == self_index) score = 0;
+		else score = 100;
+		int nextPlayer = (player + 1) % roles.size();
+		if (nextPlayer == self_index) d -= 1;
+
+		if (player == self_index) {
+			for (List<Move> jointMove: jointMoves) {
+				if(System.currentTimeMillis() >= finishBy) return score;//conservative estimate
+				MachineState nextState = machine.getNextState(state, jointMove);
+				int result = alphabeta(nextPlayer % roles.size(), nextState, alpha, beta, d);
+				if (result == 100 ||  result >= beta) return 100;
+				if (result > alpha) alpha = result;
+				if (result > score) score = result;
+			}
+		} else {
+			int num_moves = 0;
+			for (List<Move> jointMove: jointMoves) {
+				if(System.currentTimeMillis() >= finishBy) {
+					//conservative estimate
+					return (int) (score * (double)num_moves / jointMoves.size());
+				}
+				MachineState nextState = machine.getNextState(state, jointMove);
+				int result = alphabeta(nextPlayer, nextState, alpha, beta, d);
+				if (result == 0 || score <= alpha) return 0;
+				if (result < beta) beta = result;
+				if (result < score) score = result;
+				num_moves++;
+			}
+		}
+
+		return score;
+	}
+
+	private StateMachine machine;
+	private List<Role> roles;
+	private int self_index, origin_player, mob_n;
+	private double[] weights;
+	private long finishBy;
+	private boolean self_focus;
+
+	private static final int GOAL_FN = 0;
+	private static final int MOB_FN = 1;
+	private static final int FOCUS_FN = 2;
+	private static final int STATE_MOB_FN = 3;
+	private static final int COMBO_FN = 4;
+	private static final int DEFAULT = 5;
+	private static final int NUM_FNS = 3;
+
+	protected int nStepMobilityFn(int player, MachineState state, int n) throws MoveDefinitionException, TransitionDefinitionException {
+		int mobility = (int) (100 * (nStepActions(player, state, n) /
+				machine.findActions(roles.get(origin_player)).size()));
+		return mobility;
+	}
+
+	//Assumes origin_player and opponents moves are uniformly randomly distributed
+	//Consider changing to origin_player maximizing possible moves
+	protected double nStepActions(int player, MachineState state, int n) throws MoveDefinitionException, TransitionDefinitionException {
+		if (machine.isTerminal(state))
+			return 0;
+		if (n == 0) {
+			return machine.getLegalMoves(state, roles.get(origin_player)).size();
+		}
+
+		List<List<Move>> jointMoves = machine.getLegalJointMoves(state);
+		double totalMoves = 0;
+		int nextPlayer = (player + 1) % roles.size();
+		if (nextPlayer == origin_player) n-= 1;
+		for (List<Move> jointMove: jointMoves) {
+			if (System.currentTimeMillis() >= finishBy) break;
+			MachineState nextState = machine.getNextState(state, jointMove);
+			totalMoves += nStepActions(nextPlayer, nextState, n);
+		}
+		return totalMoves / jointMoves.size();
 	}
 
 	protected int nStepFocusFn(int player, MachineState state, int n) throws MoveDefinitionException, TransitionDefinitionException {
-		if (roles.size() == 1) return 0;
-		int score = 0;
-		MachineState next_state = state;
-		for (int i = player + 1; i != player; i = (i+1) % roles.size()) {
-			next_state = machine.getNextState(next_state, machine.getRandomJointMove(next_state));
-			score += 100 - nStepMobilityFn(i, next_state, n);
+		if (self_focus) return 100 - nStepMobilityFn(player, state, n); //Reduce player's mobility
+		//Reduce the average mobility of all opponents
+		double opp_focus = 0;
+		for (int i = (player + 1) % roles.size(); i != player; i = (i + 1) % roles.size()) {
+			if (System.currentTimeMillis() >= finishBy) break;
+			origin_player = i;
+			opp_focus += (100 - nStepMobilityFn(player, state, (n == 0 ? 1 : n)));
 		}
-		return score / (roles.size() - 1);
+		return (int) opp_focus / (roles.size() - 1);
 	}
+
+	private static double sigmoid(double x) {
+	    return 1 / (1 + Math.exp(-x));
+	}
+
+	//problematic to map score to {0, ..., 100} unless you know total number of states in the game
+	protected int nStepStateMobilityFn(int player, MachineState state, int n) throws MoveDefinitionException, TransitionDefinitionException {
+		HashSet<MachineState> uniqueStates = new HashSet<MachineState>();
+		statesReachable(player, state, n, uniqueStates);
+		return (int) (10 * sigmoid(uniqueStates.size() - 10));
+	}
+
 	//Only adds states which are reachable by origin_player
 	protected void statesReachable(int player, MachineState state, int n,
 			HashSet<MachineState> uniqueStates) throws MoveDefinitionException, TransitionDefinitionException {
-		if (machine.isTerminal(state) || n == 0)
+		if (machine.isTerminal(state) || n == 0 || (System.currentTimeMillis() >= finishBy))
 			return;
-
 		List<List<Move>> jointMoves = machine.getLegalJointMoves(state);
 		int nextPlayer = player + 1;
 		if (nextPlayer == origin_player) {
@@ -120,97 +204,31 @@ public class GoatsIterativeDeep extends the_men_who_stare_at_goats {
 		}
 	}
 
-	//Assumes origin_player and opponents moves are uniformly randomly distributed
-	//Consider changing to origin_player maximizing possible moves
-	protected double nStepActions(int player, MachineState state, int n) throws MoveDefinitionException, TransitionDefinitionException {
-		if (machine.isTerminal(state))
-			return 0;
-		if (n == 0)
-			return machine.getLegalMoves(state, roles.get(origin_player)).size();
-
-		List<List<Move>> jointMoves = machine.getLegalJointMoves(state);
-		double totalMoves = 0;
-		int nextPlayer = player + 1;
-		if (nextPlayer == origin_player) n-= 1;
-		for (List<Move> jointMove: jointMoves) {
-			MachineState nextState = machine.getNextState(state, jointMove);
-			totalMoves += nStepActions(nextPlayer % roles.size(), nextState, n);
-		}
-		return totalMoves / jointMoves.size();
-	}
-
 	protected int goalProxFn(int player, MachineState state) throws GoalDefinitionException {
 		return machine.getGoal(state, roles.get(self_index));
 	}
 
 	protected int weightedCombo(int player, MachineState state) throws MoveDefinitionException, GoalDefinitionException, TransitionDefinitionException {
-		int total = 0;
-		int numFns = 2;
-		for (int i = 0; i < numFns; i++) {
+		double total = 0;
+		for (int i = 0; i < NUM_FNS; i++) {
+			if (System.currentTimeMillis() >= finishBy) break;
 			total += weights[i] * evalFn(player, state, i);
 		}
-		return total / numFns;
+		return (int) total;
 	}
 
 	protected int evalFn(int player, MachineState state, int fn) throws MoveDefinitionException, GoalDefinitionException, TransitionDefinitionException {
+		origin_player = player;
 		switch (fn) {
 		case 0: return goalProxFn(player, state);
 		case 1: return nStepMobilityFn(player, state, mob_n);
 		case 2: return nStepFocusFn(player, state, mob_n);
+		//case 3: return nStepStateMobilityFn(player, state, mob_n + 1);
 		case 3: return weightedCombo(player, state);
-		default: return 0;
+		default: return 1;
 		}
 	}
-	protected int alphabeta(int player, MachineState state, int alpha, int beta, int d, int fn) throws MoveDefinitionException, GoalDefinitionException, TransitionDefinitionException {
-		if (machine.isTerminal(state)) {
-			return machine.getGoal(state, roles.get(self_index));
-		}
-		if (d == 0) {
-			return evalFn(player, state, fn);
-		}
 
-		List<List<Move>> jointMoves = machine.getLegalJointMoves(state);
-		int score;
-		if (player == self_index) score = 0;
-		else score = 100;
-		int nextPlayer = (player + 1) % roles.size();
-		if (nextPlayer == self_index) d -= 1;
-
-		if (player == self_index) {
-			for (List<Move> jointMove: jointMoves) {
-				MachineState nextState = machine.getNextState(state, jointMove);
-				int result = alphabeta(nextPlayer % roles.size(), nextState, alpha, beta, d, fn);
-				if (result == 100 ||  result >= beta) return 100;
-				if (result > alpha) alpha = result;
-				if (result > score) score = result;
-				if(System.currentTimeMillis() > finishBy) return 0;
-			}
-		} else {
-			for (List<Move> jointMove: jointMoves) {
-				MachineState nextState = machine.getNextState(state, jointMove);
-				int result = alphabeta(nextPlayer, nextState, alpha, beta, d, fn);
-				if (result == 0 || score <= alpha) return 0;
-				if (result < beta) beta = result;
-				if (result < score) score = result;
-				if(System.currentTimeMillis() > finishBy) return 0;
-			}
-		}
-
-		return score;
-
-	}
-
-	private StateMachine machine;
-	private List<Role> roles;
-	private int self_index, origin_player, mob_n;
-	private double[] weights;
-	private long finishBy;
-
-	private static final int GOAL_FN = 0;
-	private static final int MOBILITY_FN = 1;
-	private static final int FOCUS_FN = 2;
-	private static final int WEIGHTED_FN = 3;
-	private static final int DEFAULT = 4;
 	@Override
 	public Move stateMachineSelectMove(long timeout)
 			throws TransitionDefinitionException, MoveDefinitionException,
