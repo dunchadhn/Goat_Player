8efe0b022eb248a46d55ba0d6b4ca7cc83015b05
George Preudhomme <Achilles@rescomp-16-289183.stanford.edu>
2017-05-14 16:16:34 -0700

    PropNet

---
+++ ggp-base-master/src/main/java/org/ggp/base/player/gamer/statemachine/SampleLegalGamer2.java
@@ -0,0 +1,88 @@
+package org.ggp.base.player.gamer.statemachine;
+
+import java.util.List;
+
+import org.ggp.base.player.gamer.event.GamerSelectedMoveEvent;
+import org.ggp.base.util.statemachine.MachineState;
+import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.cache.CachedStateMachine;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
+
+/**
+ * SampleLegalGamer is a minimal gamer which always plays the first
+ * legal move it identifies, regardless of the state of the game.
+ *
+ * For your first players, you should extend the class SampleGamer
+ * The only function that you are required to override is :
+ * public Move stateMachineSelectMove(long timeout)
+ *
+ */
+public final class SampleLegalGamer2 extends SampleGamer
+{
+
+
+	private StateMachine machine2, machine;
+	@Override
+	public void stateMachineMetaGame(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException
+	{
+		machine = new CachedStateMachine(new ProverStateMachine());
+		machine.initialize(getMatch().getGame().getRules());
+		MachineState state = machine.getInitialState();
+		machine2 = getStateMachine();
+		MachineState state2 = machine2.getInitialState();
+		if(!state.equals(state2)) {
+			System.out.println("Correct: " + state.getContents());
+			System.out.println("Wrong: " + state2.getContents());
+			System.out.println("FAILURE");
+		}
+	}
+	/**
+	 * This function is called at the start of each round
+	 * You are required to return the Move your player will play
+	 * before the timeout.
+	 *
+	 */
+	@Override
+	public Move stateMachineSelectMove(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException
+	{
+		// We get the current start time
+		long start = System.currentTimeMillis();
+
+		/**
+		 * We put in memory the list of legal moves from the
+		 * current state. The goal of every stateMachineSelectMove()
+		 * is to return one of these moves. The choice of which
+		 * Move to play is the goal of GGP.
+		 */
+
+
+		List<Move> moves2 = machine2.getLegalMoves(getCurrentState(), getRole());
+		List<Move> moves = machine.getLegalMoves(getCurrentState(), getRole());
+		if (moves != moves2) {
+			System.out.println(getRole());
+			System.out.println("Correct: " + moves);
+			System.out.println("Wrong: " + moves2);
+		}
+		assert(moves == moves2);
+
+		// SampleLegalGamer is very simple : it picks the first legal move
+		Move selection = moves.get(0);
+
+		// We get the end time
+		// It is mandatory that stop<timeout
+		long stop = System.currentTimeMillis();
+
+		/**
+		 * These are functions used by other parts of the GGP codebase
+		 * You shouldn't worry about them, just make sure that you have
+		 * moves, selection, stop and start defined in the same way as
+		 * this example, and copy-paste these two lines in your player
+		 */
+		notifyObservers(new GamerSelectedMoveEvent(moves, selection, stop - start));
+		return selection;
+	}
+}
