24752329e34d77ae5a23a4d1e9c88d52a6d7e20b
Dunchadhn Lyons <dunlyons@stanford.edu>
2017-05-20 17:08:25 -0700

    Threadpool ready for test

---
+++ ggp-base-master/src/main/java/org/ggp/base/player/gamer/statemachine/BitNode.java
@@ -0,0 +1,28 @@
+package org.ggp.base.player.gamer.statemachine;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.ggp.base.util.statemachine.Move;
+
+public class BitNode {
+
+	public static int nodeCount = 0;
+
+	BitNode(BitMachineState state) {
+		this.state = state;
+		this.isTerminal = false;
+		this.children = new HashMap<List<Move>, BitNode>();
+		this.legalJointMoves = new HashMap<Move, List<List<Move>>>();
+		this.utility = 0;
+		this.visits = 0;
+		++nodeCount;
+	}
+	public BitMachineState state;
+	public double utility;
+	public double visits;
+	public List<Move> legalMoves;
+	public HashMap<List<Move>, BitNode> children;
+	public HashMap<Move, List<List<Move>>> legalJointMoves;
+	public boolean isTerminal;
+}
--- ggp-base-master/src/main/java/org/ggp/base/player/gamer/statemachine/Bit_MCTS_threadpool.java
+++ ggp-base-master/src/main/java/org/ggp/base/player/gamer/statemachine/Bit_MCTS_threadpool.java
@@ -9,26 +9,24 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 import org.ggp.base.util.statemachine.BitStateMachine;
-import org.ggp.base.util.statemachine.MachineState;
 import org.ggp.base.util.statemachine.Move;
 import org.ggp.base.util.statemachine.Role;
-import org.ggp.base.util.statemachine.StateMachine;
 import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
 
 
-public class Bit_MCTS_threadpool extends the_men_who_stare_at_goats {
+public class Bit_MCTS_threadpool extends BIT_the_men_who_stare_at_goats {
 	private BitStateMachine machine;
 	private List<BitStateMachine> machines;
 	private List<Role> roles;
 	private int self_index, num_threads, depthCharges;
 	private long finishBy;
-	private Node root;
-	private List<Node> path;
+	private BitNode root;
+	private List<BitNode> path;
 	private ExecutorService executor;
 	private List<Future<?>> futures;
-	private Node n;
+	private BitNode n;
 	private Lock lock;
 
 	private static final double C_CONST = 50;
@@ -46,11 +44,11 @@ public class Bit_MCTS_threadpool extends the_men_who_stare_at_goats {
 		machine = getStateMachine();
 		roles = machine.getRoles();
 		self_index = roles.indexOf(getRole());
-		root = new Node(machine.getInitialState());
+		root = new BitNode(machine.getInitialState());
 		Expand(root);
 		num_threads = Runtime.getRuntime().availableProcessors() * 12;
 		executor = Executors.newFixedThreadPool(num_threads);
-		machines = new ArrayList<StateMachine>();
+		machines = new ArrayList<BitStateMachine>();
 		for(int i = 0; i < num_threads; i++) {
 			machines.add(getInitialStateMachine());
 			machines.get(i).initialize(getMatch().getGame().getRules());
@@ -72,11 +70,11 @@ public class Bit_MCTS_threadpool extends the_men_who_stare_at_goats {
 
 	protected void initializeMCTS() throws MoveDefinitionException, TransitionDefinitionException, InterruptedException {
 		for (int i = 0; i < machines.size(); ++i) machines.get(i).doPerMoveWork();
-		MachineState currentState = getCurrentState();
+		BitMachineState currentState = getCurrentState();
 		if (root == null) System.out.println("NULL ROOT");
 		if (root.state.equals(currentState)) return;
 		for (List<Move> jointMove : machine.getLegalJointMoves(root.state)) {
-			MachineState nextState = machine.getNextState(root.state, jointMove);
+			BitMachineState nextState = machine.getNextState(root.state, jointMove);
 			if (currentState.equals(nextState)) {
 				root = root.children.get(jointMove);
 				if (root == null) System.out.println("NOT IN MAP");
@@ -84,7 +82,7 @@ public class Bit_MCTS_threadpool extends the_men_who_stare_at_goats {
 			}
 		}
 		System.out.println("ERROR. Current State not in tree");
-		root = new Node(currentState);
+		root = new BitNode(currentState);
 	}
 
 	protected Move MCTS() throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException, InterruptedException, ExecutionException {
@@ -101,7 +99,7 @@ public class Bit_MCTS_threadpool extends the_men_who_stare_at_goats {
 		long time_elapsed = 0;
 		while (System.currentTimeMillis() + average_time < finishBy) {
 			time_elapsed = System.currentTimeMillis();
-			path = new ArrayList<Node>();
+			path = new ArrayList<BitNode>();
 			futures = new ArrayList<> ();
 			lock = new ReentrantLock();
 			path.add(root);
@@ -140,14 +138,14 @@ public class Bit_MCTS_threadpool extends the_men_who_stare_at_goats {
 	    }
 	}
 
-	protected Move bestMove(Node n) throws MoveDefinitionException {
+	protected Move bestMove(BitNode n) throws MoveDefinitionException {
 		double maxValue = Double.NEGATIVE_INFINITY;
 		Move maxMove = n.legalMoves.get(0);
 		for(Move move: n.legalMoves) {
 			double minValue = Double.POSITIVE_INFINITY;
 			double visits = 0;
 			for (List<Move> jointMove : n.legalJointMoves.get(move)) {
-				Node succNode = n.children.get(jointMove);
+				BitNode succNode = n.children.get(jointMove);
 				if (succNode.visits != 0) {
 					double nodeValue = succNode.utility / succNode.visits;
 					if (nodeValue < minValue) {
@@ -166,8 +164,8 @@ public class Bit_MCTS_threadpool extends the_men_who_stare_at_goats {
 		return maxMove;
 	}
 
-	protected void Backpropogate(double val, List<Node> path) {
-		Node nod = path.get(path.size() - 1);
+	protected void Backpropogate(double val, List<BitNode> path) {
+		BitNode nod = path.get(path.size() - 1);
 		if (machine.isTerminal(nod.state)) {
 			nod.isTerminal = true;
 		}
@@ -178,26 +176,26 @@ public class Bit_MCTS_threadpool extends the_men_who_stare_at_goats {
 		}
 	}
 
-	protected double Playout(Node n) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException {
+	protected double Playout(BitNode n) throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException {
 		int threadId = (int) (Thread.currentThread().getId() % num_threads);
-		StateMachine machine = machines.get(threadId);
-		MachineState state = n.state;
+		BitStateMachine machine = machines.get(threadId);
+		BitMachineState state = n.state;
 		while(!machine.isTerminal(state)) {
 			state = machine.getRandomNextState(state);
 		}
 		return machine.getGoal(state, roles.get(self_index));
 	}
 
-	protected void Select(Node n, List<Node> path) throws MoveDefinitionException {
+	protected void Select(BitNode n, List<BitNode> path) throws MoveDefinitionException {
 		if (machine.isTerminal(n.state)) return;
 		if (n.children.isEmpty()) return;
 		double maxValue = Double.NEGATIVE_INFINITY;
-		Node maxChild = null;
+		BitNode maxChild = null;
 		for(Move move: n.legalMoves) {
 			double minValue = Double.NEGATIVE_INFINITY;
-			Node minChild = null;
+			BitNode minChild = null;
 			for (List<Move> jointMove : n.legalJointMoves.get(move)) {
-				Node succNode = n.children.get(jointMove);
+				BitNode succNode = n.children.get(jointMove);
 				if (succNode.visits == 0) {
 					path.add(succNode);
 					return;
@@ -219,24 +217,24 @@ public class Bit_MCTS_threadpool extends the_men_who_stare_at_goats {
 	}
 
 
-	protected double uctMin(Node n, double parentVisits) {
+	protected double uctMin(BitNode n, double parentVisits) {
 		double value = n.utility / n.visits;
 		return -value + C_CONST * Math.sqrt(Math.log(parentVisits) / n.visits);
 	}
 
-	protected double uctMax(Node n, double parentVisits) {
+	protected double uctMax(BitNode n, double parentVisits) {
 		double value = n.utility / n.visits;
 		return value + C_CONST * Math.sqrt(Math.log(parentVisits) / n.visits);
 	}
 
-	protected void Expand(Node n, List<Node> path) throws MoveDefinitionException, TransitionDefinitionException {
+	protected void Expand(BitNode n, List<BitNode> path) throws MoveDefinitionException, TransitionDefinitionException {
 		if (n.children.isEmpty() && !machine.isTerminal(n.state)) {
 			n.legalMoves = machine.getLegalMoves(n.state, roles.get(self_index));
 			for (Move move: n.legalMoves) {
 				n.legalJointMoves.put(move, new ArrayList<List<Move>>());
 			}
 			for (List<Move> jointMove: machine.getLegalJointMoves(n.state)) {
-				Node child = new Node(machine.getNextState(n.state, jointMove));
+				BitNode child = new BitNode(machine.getNextState(n.state, jointMove));
 				n.legalJointMoves.get(jointMove.get(self_index)).add(jointMove);
 				n.children.put(jointMove, child);
 			}
@@ -246,14 +244,14 @@ public class Bit_MCTS_threadpool extends the_men_who_stare_at_goats {
 		}
 	}
 
-	protected void Expand(Node n) throws MoveDefinitionException, TransitionDefinitionException {//Assume only expand from max node
+	protected void Expand(BitNode n) throws MoveDefinitionException, TransitionDefinitionException {//Assume only expand from max node
 		if (n.children.isEmpty() && !machine.isTerminal(n.state)) {
 			n.legalMoves = machine.getLegalMoves(n.state, roles.get(self_index));
 			for (Move move: n.legalMoves) {
 				n.legalJointMoves.put(move, new ArrayList<List<Move>>());
 			}
 			for (List<Move> jointMove: machine.getLegalJointMoves(n.state)) {
-				Node child = new Node(machine.getNextState(n.state, jointMove));
+				BitNode child = new BitNode(machine.getNextState(n.state, jointMove));
 				n.legalJointMoves.get(jointMove.get(self_index)).add(jointMove);
 				n.children.put(jointMove, child);
 			}
