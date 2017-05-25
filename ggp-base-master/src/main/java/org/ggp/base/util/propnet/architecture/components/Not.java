cbdd98a154365a61dbfcb06a24718ae61fc2d8cf
George Preudhomme <Achilles@DN51ul3r.SUNet>
2017-05-24 22:12:16 -0700

    progress is slow

--- ggp-base-master/src/main/java/org/ggp/base/util/propnet/architecture/components/Not.java
+++ ggp-base-master/src/main/java/org/ggp/base/util/propnet/architecture/components/Not.java
@@ -30,17 +30,18 @@ public final class Not extends Component
 
 	@Override
 	public String bitString(int compValue, long compInfo, int[] connecTable) {
-		assert numInputs(compInfo) == 1;
 		boolean currVal = get_current_value(compValue);
 		if (currVal) {
 			if (compValue != 0xFFFF_FFFF) {
-				System.out.println("NOT is true but not 0xFFFF");
+				System.out.println("NOT is true but not 0xFFFFFFFF");
+				System.out.println(Integer.toHexString(compValue));
 				System.exit(0);
 			}
 		}
 		else {
 			if (compValue != 0) {
 				System.out.println("NOT is false but not 0");
+				System.out.println(Integer.toHexString(compValue));
 				System.exit(0);
 			}
 		}
